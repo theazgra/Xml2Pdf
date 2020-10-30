cfb3bc5f6a0ed755a2c39857500593bd7362cc6d
Vojtech Moravec <theazgra@gmail.com>
2020-10-30 11:06:12 +0100

    Colored console output utility.

---
+++ Xml2Pdf/Xml2Pdf/Utilities/ColorConsole.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Xml2Pdf.Utilities
+{
+    internal static class ColorConsole
+    {
+        internal static void WriteLine(ConsoleColor color, string format, params object[] parameters)
+        {
+            WriteLine(color, string.Format(format, parameters));
+        }
+
+        internal static void WriteLine(ConsoleColor color, string str)
+        {
+            var originalColor = Console.ForegroundColor;
+            try
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(str);
+            }
+            finally
+            {
+                Console.ForegroundColor = originalColor;
+            }
+        }
+    }
+}
